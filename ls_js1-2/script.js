c171813925db2df503a27655b535dd2c2b31834b
Andrey <kluy@i.ua>
2017-01-11 17:34:42 +0200

    js_lesson1-2_2

--- ls_js1-2/script.js
+++ ls_js1-2/script.js
@@ -2,18 +2,39 @@
  * Created by TERM2 on 13.12.2016.
  */
 
-function pow() {
-    j = a;
-    var i=1;
-    while (i < b){
-        a = a * j;
-        i++
+function pow(number,power) {
+    j = number;
+    while (power < 0) {
+        power = prompt('Введите степень > или = 0');
     }
-    alert(a);
-    console.log('result', a);
+    if (power > 0) {
+        var i=1;
+        while (i < power){
+            number = number * j;
+            i++
+        }
+    } else if (power == 0){
+        number = 1;
+    }
+    alert(number);
+    console.log('result', number);
 }
 
-var a = prompt('Введите число');
-var b = prompt('Введите степень');
+number = prompt('Введите число');
 
-pow();
+Number.isNaN = Number.isNaN || function (number) {
+        return typeof number === 'number' && isNaN(number)
+    }
+while (isNaN(number) == true) {
+    number = prompt('Число введено неверно, введите число');
+} if (isNaN(number) == false) {
+    power = prompt('Введите степень');
+    Number.isNaN = Number.isNaN || function (power) {
+            return typeof power === 'number' && isNaN(power)
+        }
+    while (isNaN(power) == true) {
+        power = prompt('Степень введена неверно, введите число');
+    } if (isNaN(power) == false) {
+        pow(number,power);
+    }
+}
