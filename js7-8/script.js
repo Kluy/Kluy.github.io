ada910be2afa3fb448f2a75da91e189866809130
Andrey <kluy@i.ua>
2017-02-06 13:12:00 +0200

    js7-8_2

--- js7-8/script.js
+++ js7-8/script.js
@@ -2,52 +2,60 @@
  * Created by TERM2 on 31.01.2017.
  */
 
-$(function form () {
-    var $header = $('.header');
+$(function () {
 
-    $header.on('click',function () {
-        $header
+    $('.header').mousedown(function() {
+        $('.header:not(.red)').css('border','1px solid gray');
+        $(this).css('border','1px solid #52abcb');
+        $('.active').css('border','1px solid #4f92ff');
+    });
+});
+
+$(function () {
+
+    $('.header').on('click',function () {
+
+        $('.header')
+            .removeClass('active')
+            .removeClass('red')
             .css({
                 backgroundColor: 'gainsboro',
                 borderColor: 'gray',
-                cursor: 'pointer'});
-
-        $header.hover(
-            function() {
-                $(this).css('backgroundColor','#ececec');
-            }, function() {
-                $(this).css('backgroundColor','gainsboro');
-            }
-        );
+                cursor: 'pointer'})
+            .hover(
+                function() {
+                    $(this).css('backgroundColor','#ececec');
+                }, function() {
+                    $(this).css('backgroundColor','gainsboro');
+            });
 
         $(this)
             .css({
                 backgroundColor:'#79abfc',
                 borderColor: '#4f92ff',
                 cursor: 'text'})
-            .off('mouseenter mouseleave');
+            .off('mouseenter mouseleave')
+            .off('mousedown')
+            .addClass('red');
 
-        var $text = $(this).siblings('.par');
         $('div.par').hide();
-        $text.show();
+        $(this).siblings('.par').show();
     });
 });
 
-$(function form () {
-    var $input = $('input');
+$(function () {
 
-    $input.hover(function () {
-        var $tooltips = $(this).siblings('.input');
-        $tooltips.css({display: 'inline-block'})
+    $('input').hover(function () {
+        $(this)
+            .siblings('.input')
+            .css('display','inline-block')
     },  function () {
-        var $tooltips = $(this).siblings('.input');
-        $tooltips.css({display: 'none'})
+        $(this)
+            .siblings('.input')
+            .css('display','none')
     });
 
-    var $tooltipsText = $('.input');
-
-    var $button = $('button');
-    $button.on('click', function () {
-        $tooltipsText.css({display: 'inline-block'})
+    $('button').on('click', function () {
+        $('.input').css({display: 'inline-block'})
     });
 });
