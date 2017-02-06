6493793b5f8ddce20b8b468e17fc22bb0e54564d
Andrey <kluy@i.ua>
2017-02-06 13:26:55 +0200

    js7-8_3

--- js7-8/script.js
+++ js7-8/script.js
@@ -4,43 +4,37 @@
 
 $(function () {
 
-    $('.header').mousedown(function() {
-        $('.header:not(.red)').css('border','1px solid gray');
-        $(this).css('border','1px solid #52abcb');
-        $('.active').css('border','1px solid #4f92ff');
-    });
-});
-
-$(function () {
-
-    $('.header').on('click',function () {
-
-        $('.header')
-            .removeClass('active')
-            .removeClass('red')
-            .css({
-                backgroundColor: 'gainsboro',
-                borderColor: 'gray',
-                cursor: 'pointer'})
-            .hover(
-                function() {
-                    $(this).css('backgroundColor','#ececec');
-                }, function() {
-                    $(this).css('backgroundColor','gainsboro');
-            });
-
-        $(this)
-            .css({
-                backgroundColor:'#79abfc',
-                borderColor: '#4f92ff',
-                cursor: 'text'})
-            .off('mouseenter mouseleave')
-            .off('mousedown')
-            .addClass('red');
-
-        $('div.par').hide();
-        $(this).siblings('.par').show();
-    });
+    $('.header')
+        .on('click',function () {
+            $('.header')
+                .removeClass('active')
+                .removeClass('red')
+                .css({
+                    backgroundColor: 'gainsboro',
+                    borderColor: 'gray',
+                    cursor: 'pointer'})
+                .hover(
+                    function() {
+                        $(this).css('backgroundColor','#ececec');
+                    }, function() {
+                        $(this).css('backgroundColor','gainsboro');
+                    });
+            $(this)
+                .css({
+                    backgroundColor:'#79abfc',
+                    borderColor: '#4f92ff',
+                    cursor: 'text'})
+                .off('mouseenter mouseleave')
+                .off('mousedown')
+                .addClass('red')
+            $('div.par').hide();
+            $(this).siblings('.par').show();
+        })
+        .mousedown(function() {
+            $('.header:not(.red)').css('border','1px solid gray');
+            $(this).css('border','1px solid #52abcb');
+            $('.active').css('border','1px solid #4f92ff');
+        })
 });
 
 $(function () {
