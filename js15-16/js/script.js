84a16ccd065c0d2253ecc4c6b2556e26eac53248
Andrey <kluy@i.ua>
2017-04-12 17:10:10 +0300

    js15-16

--- js15-16/js/script.js
+++ js15-16/js/script.js
@@ -46,15 +46,58 @@ $(function () {
 
         var newQuestion = new Test($('#question').val(), answers, $('#rightAnswer').val());
 
+        console.log('newQuestion', newQuestion);
+
+
         questionsList.push(newQuestion);
 
         var questionsListSave = JSON.stringify(questionsList);
+
+        console.log('questionsListSave', questionsListSave);
+
         localStorage.setItem('test', questionsListSave);
 
         var testFromStorage = localStorage.getItem('test');
+
+        console.log('testFromStorage', testFromStorage);
+
         var testFromStorageObj = JSON.parse(testFromStorage);
 
         console.log('testFromStorageObj', testFromStorageObj);
-        console.log('testFromStorageObj', testFromStorageObj[2]);
+        console.log('testFromStorageObjN', testFromStorageObj[2]);
     }
 });
+
+// $(function () {
+//
+//     var button = $('<button class="searchButton">Поиск</button>');
+//
+//     $('body').append($('<div>Введите поисковый запрос:</div>'))
+//              .append($('<input type="text" id="search">'))
+//              .append(button);
+//
+//     $('.searchButton').on('click', search);
+//
+//     function search (){
+//
+//         // var keyWord = document.getElementById('text').value;
+//         var keyWord = $('#search').val();
+//         var api_key = '4870172-a6cd6d722569b9e8587abde34';
+//         var url = "https://pixabay.com/api/?key=" + api_key + "&q=" + keyWord;
+//
+//         // var url = "https://api.tenor.co/v1/search?key=LIVDSRZULELA&tag=goodluck";
+//         $.ajax({
+//             url: url,
+//             success: function(data){
+//                 // var context = data;
+//                 console.log('data', data);
+//                 var img = $('<img src="" alt="">').appendTo('body');
+//                     img.attr('src', data.hits[1].previewURL);
+//                 // $('body').append('<img src="" alt="">');
+//                 // $('img').append(data.results[1].url);
+//                 console.log('data:url', data.hits[1].previewURL);
+//                 console.log('data:obj', data.hits);
+//             }
+//         });
+//     };
+// });
