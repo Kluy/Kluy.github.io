9c6d08398eb660cade0c98bd3c3fa1209ffcd61c
Andrey <kluy@i.ua>
2017-10-02 13:38:30 +0300

    test_2

--- test/src/js/script.js
+++ test/src/js/script.js
@@ -13,32 +13,25 @@ $(function () {
 
     function search (){
 
-        $('a').remove();
+        // $('a').remove();
         var keyWord = $('input').val();
         var url = "https://pixabay.com/api/?key=4870172-a6cd6d722569b9e8587abde34&q=" + keyWord;
         $.ajax({
             url: url,
             success: function(data){
 
-                for (var i = 0; i < data.hits.length; i++) {
-                    var href = $('<a href=""></a>');
-                    href.appendTo('grid')
-                        .attr('href', data.hits[i].pageURL);
-                    var img = $('<img src="" alt="">');
-                    href.append(img);
-                    img.attr('src', data.hits[i].previewURL);
-                }
 
-                    // for (var i = 0; i < 7; i++) {
-                    //     var gridItem = $('<div class="grid-item"></div>');
-                    //     gridItem.appendTo('.grid');
-                    //     var href = $('<a href=""></a>');
-                    //     href.appendTo('.grid-item')
-                    //         .attr('href', data.hits[i].pageURL);
-                    //     var img = $('<img src="" alt="">');
-                    //     href.append(img);
-                    //     img.attr('src', data.hits[i].webformatURL);
-                    //     }
+
+                    for (var i = 0; i < 7; i++) {
+                        var gridItem = $('<div class="grid-item"></div>');
+                        gridItem.appendTo('.grid');
+                        var href = $('<a href=""></a>');
+                        href.appendTo('.grid-item')
+                            .attr('href', data.hits[i].pageURL);
+                        var img = $('<img src="" alt="">');
+                        href.append(img);
+                        img.attr('src', data.hits[i].webformatURL);
+                        }
             },
 
             error: function () {
