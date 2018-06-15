e675e497cb410eee33555247f9d81817c0d23994
Andrey <kluy@i.ua>
2018-06-15 13:50:51 +0300

    mass_0615

--- massage/send-mail.php
+++ massage/send-mail.php
@@ -1,18 +1,19 @@
 <?php
 
-$to = 'user@yourdomain.com'; // Change your email address
+$to = 'info@massashka.ru'; // Change your email address
 
 
 $name = $_POST['name'];
 $subject = $_POST['subject'];
 $email = $_POST['email'];
+$tel = $_POST['tel'];
 $message = $_POST['message'];
 
 
 // Email Submit
 // Note: filter_var() requires PHP >= 5.2.0
- if ( isset($email) && isset($name) && isset($subject) && isset($message) && filter_var($email, FILTER_VALIDATE_EMAIL) ) {
- 
+ if ( isset($email) && isset($name) && isset($tel) && isset($subject) && isset($message) && filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+
   // detect & prevent header injections
   $test = "/(content-type|bcc:|cc:|to:)/i";
   foreach ( $_POST as $key => $val ) {
@@ -22,14 +23,15 @@ $message = $_POST['message'];
   }
 
 $body = <<<EMAIL
-subject : $subject
+Тема: $subject
   
-My name is, $name.
+Меня зовут, $name.
 
 $message
 
-From : $name
-Email : $email
+Имя: $name
+E-mail: $email
+Тел.: $tel
 
 EMAIL;
   
@@ -39,8 +41,8 @@ $header = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
   //
- // mail( $to , $_POST['subject'], $_POST['message'], $headers );
- mail($to, $subject, $body, $header);
+ // mb_send_mail( $to , $_POST['subject'], $_POST['message'], $headers );
+ mb_send_mail($to, $subject, $body, $header);
   //      ^
   //  Replace with your email 
 }
